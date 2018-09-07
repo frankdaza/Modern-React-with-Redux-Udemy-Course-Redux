d27473eaf1694246b8f050637e68cca187756f83
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-06 21:59:48 -0500

    Creating my first Reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import BookReducer from './BookReducer';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state: BookReducer
 });
 
 export default rootReducer;
